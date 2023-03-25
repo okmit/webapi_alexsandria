f296d40b63244b74be0eaa911b57b9990c66a656
Oleg Kmit <oleg.kmit@gmail.com>
2023-03-25 14:40:47 +0200

    Create Sorter, Filter and EagerLoader

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   rescue_from QueryBuilderError, with: :query_builder_error
+  
 
   protected
 
@@ -12,10 +13,18 @@ class ApplicationController < ActionController::API
     }
   end
 
+  def sort(scope)
+    Sorter.new(scope, params).sort
+  end
+
   def paginate(scope)
     paginator = Paginator.new(scope, request.query_parameters, current_url)
     response.headers['Link'] = paginator.links
     paginator.paginate
+  end
+
+  def filter(scope)
+    Filter.new(scope, params.to_unsafe_hash).filter
   end
 
   def current_url
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def index
-    books = paginate(Book.all).map do |book|
+    books = filter(sort(paginate(Book.all))).map do |book|
       FieldPicker.new(BookPresenter.new(book, params)).pick
     end
 
